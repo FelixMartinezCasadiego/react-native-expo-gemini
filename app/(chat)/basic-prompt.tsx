8a918a5abd26b4bb3213fc4a2d7661452c229c43
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-08-25 20:46:11 +0200

    feat: add process indicator in ChatMessages.tsx file

--- app/(chat)/basic-prompt.tsx
+++ app/(chat)/basic-prompt.tsx
@@ -7,10 +7,11 @@ import { Layout } from "@ui-kitten/components";
 const BasicPromptScreen = () => {
   const messages = useBasicPrompt((state) => state.messages);
   const addMessage = useBasicPrompt((state) => state.addMessage);
+  const geminiWriting = useBasicPrompt((state) => state.geminiWriting);
 
   return (
     <Layout style={{ flex: 1 }}>
-      <ChatMessages messages={messages} />
+      <ChatMessages messages={messages} isGeminiWriting={geminiWriting} />
 
       <CustomInputBox onSendMessage={(message) => addMessage(message)} />
     </Layout>
